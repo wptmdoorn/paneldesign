16e966b944b66a7f4d4bb0d426adcc9a5acb6cb3
Faris Naji <fnaji@pamgene.com>
2021-07-22 08:03:49 +0100

    added code

---
+++ R/rscore.R
@@ -0,0 +1,36 @@
+m1=1
+m2=0.2
+m3=0.5
+
+m1m2=1
+m1m3=0
+m2m1=1
+
+m2m3=0
+m3m1=0
+m3m2=0
+
+f1=1
+f2=0.8
+f3=0.2
+
+f1f2=0.9	
+f1f3=0.7	
+f2f1=0.2	
+
+f2f3=0.5
+f3f1=0.2
+f3f2=0.1	
+
+rscore = function(mA, mB,
+                  fA, fB,
+                  SAB, SBA,
+                  mAB) {
+  
+    delta = abs(mA*fA - mB*fB)
+    spread = 2
+    total = 3
+}
+
+s1 = rscore(m1,m2,f1,f2,f1f2,f2f1,m1m2)
+
